6c19e145ec8bab25e9241a9695c972262caff3bd
dilmin-dtecknow <fernandodoloman42@gmail.com>
2026-07-07 12:42:44 +0530

    build: initialize spring boot backend

---
+++ backend/src/test/java/com/dilmin/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilmin.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
