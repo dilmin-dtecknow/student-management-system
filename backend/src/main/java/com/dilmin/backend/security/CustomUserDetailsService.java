c7b80ac289ef66a9531380ac20af28a89d1aa477
dilmin-dtecknow <fernandodoloman42@gmail.com>
2026-07-10 00:03:29 +0530

    feat(auth): Update custom user details service

--- backend/src/main/java/com/dilmin/backend/security/CustomUserDetailsService.java
+++ backend/src/main/java/com/dilmin/backend/security/CustomUserDetailsService.java
@@ -1,4 +1,25 @@
 package com.dilmin.backend.security;
 
-public class CustomUserDetailsService {
+import com.dilmin.backend.entity.User;
+import com.dilmin.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found"));
+
+
+
+        return new CustomUserDetails(user);
+    }
 }
