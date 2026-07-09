d6fdb6a33f6eb0e6b1a1480587d30617a7462798
dilmin-dtecknow <fernandodoloman42@gmail.com>
2026-07-10 01:16:54 +0530

    feat(auth): configure Spring Security

--- backend/src/main/java/com/dilmin/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/dilmin/backend/config/SecurityConfig.java
@@ -1,24 +1,37 @@
 package com.dilmin.backend.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final AuthenticationProvider authenticationProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //no create server session
+
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST,"/api/**").permitAll()
+                        .requestMatchers("/api/v1/auth/**").permitAll()
+//                        .requestMatchers(HttpMethod.GET,"/api/courses","api/students","api/users").permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults());
+                .authenticationProvider(authenticationProvider);
 
         return http.build();
     }
