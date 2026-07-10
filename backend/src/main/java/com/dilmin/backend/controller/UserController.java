dc39021a26a4ed84dec22fe5301726f6d2855d01
dilmin-dtecknow <fernandodoloman42@gmail.com>
2026-07-10 13:29:20 +0530

    fet(auth): implement role base access for user end points

--- backend/src/main/java/com/dilmin/backend/controller/UserController.java
+++ backend/src/main/java/com/dilmin/backend/controller/UserController.java
@@ -4,6 +4,7 @@ import com.dilmin.backend.entity.User;
 import com.dilmin.backend.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,26 +16,31 @@ import java.util.UUID;
 public class UserController {
     private final UserService userService;
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping
     public User saveUser(@RequestBody User user) {
         return userService.saveUser(user);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN','STAFF')")
     @GetMapping
     public List<User> getAllUsers() {
         return userService.getUsers();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{id}")
     public User getUserById(@PathVariable UUID id) {
         return userService.getUserById(id);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public User updateUser(@PathVariable UUID id,@RequestBody @Valid User user) {
         return userService.updateUser(id, user);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public void deleteUserById(@PathVariable UUID id) {
         userService.deleteUser(id);
